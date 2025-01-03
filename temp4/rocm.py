e299bf2a218a29c516ef5b63123997319f567e1a
tastan <mehmet.tastan@tai.com.tr>
2025-01-03 11:34:02 +0300

    delete all files

--- temp4/rocm.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-print("GPU Available:", tf.config.list_physical_devices('GPU'))
-a = tf.constant([1.0, 2.0, 3.0])
-b = tf.constant([4.0, 5.0, 6.0])
-c = a + b
-print("Result", c)
